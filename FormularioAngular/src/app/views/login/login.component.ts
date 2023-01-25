a9628ba5b06e55ec1bcdd22e970e44fbfe4cb16a
czkirilo <caiquez.kirilo@hotmail.com>
2023-01-24 21:15:44 -0300

    a

---
+++ FormularioAngular/src/app/views/login/login.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/models/user';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+
+  }
+  userModel = new User()
+
+  OnSubmit(){
+    console.log(this.userModel)
+  }
+
+}
